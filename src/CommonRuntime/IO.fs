2f247e4655d3c50c6b1dc70ce88a4d2a1df0c174
Max Hirschhorn <max.hirschhorn@mongodb.com>
2014-08-31 16:53:02 -0400

    Adapt CommonRuntime/IO.fs from FSharp.Data.
    Want to be able to resolve files and still get contents as a Stream.

---
+++ src/CommonRuntime/IO.fs
@@ -0,0 +1,58 @@
+namespace BsonProvider.Runtime
+
+open System
+open System.IO
+
+/// Helper functions called from the generated code for working with files
+module internal IO =
+
+    let isWeb (uri:Uri) = uri.IsAbsoluteUri && not uri.IsUnc && uri.Scheme <> Uri.UriSchemeFile
+
+    type UriResolutionType =
+        | DesignTime
+        | Runtime
+        | RuntimeInFSI
+
+    type UriResolver =
+
+        { ResolutionType : UriResolutionType
+          DefaultResolutionFolder : string
+          ResolutionFolder : string }
+
+        /// Resolve the absolute location of a file (or web URL) according to the rules
+        /// used by standard F# type providers as described here:
+        /// https://github.com/fsharp/fsharpx/issues/195#issuecomment-12141785
+        ///
+        ///  * if it is web resource, just return it
+        ///  * if it is full path, just return it
+        ///  * otherwise.
+        ///
+        ///    At design-time:
+        ///      * if the user specified resolution folder, use that
+        ///      * otherwise use the default resolution folder
+        ///    At run-time:
+        ///      * if the user specified resolution folder, use that
+        ///      * if it is running in F# interactive (config.IsHostedExecution)
+        ///        use the default resolution folder
+        ///      * otherwise, use 'CurrentDomain.BaseDirectory'
+        /// returns an absolute uri * isWeb flag
+        member x.Resolve(uri:Uri) =
+          if uri.IsAbsoluteUri then uri, isWeb uri
+          else
+            let root =
+              match x.ResolutionType with
+              | DesignTime -> if String.IsNullOrEmpty x.ResolutionFolder
+                              then x.DefaultResolutionFolder
+                              else x.ResolutionFolder
+
+              | RuntimeInFSI -> x.DefaultResolutionFolder
+              | Runtime -> AppDomain.CurrentDomain.BaseDirectory.TrimEnd('\\', '/')
+
+            Uri(Path.Combine(root, uri.OriginalString), UriKind.Absolute), false
+
+        member x.TryResolveToPath(uri:Uri) =
+            match x.Resolve uri with
+            | _, true -> None
+            | uri, false ->
+                let path = uri.OriginalString.Replace(sprintf "%s://" Uri.UriSchemeFile, "")
+                Some path
