618beda8410846b468191b140f5bc9a3f186f2c7
Max Hirschhorn <max.hirschhorn@mongodb.com>
2014-08-31 09:33:24 -0400

    [WIP] Add expected signature tests.

--- tests/BsonProvider.DesignTime.Tests/SignatureTests.fs
+++ tests/BsonProvider.DesignTime.Tests/SignatureTests.fs
@@ -26,7 +26,6 @@ let expectedDirectory = sourceDirectory ++ "expected"
 let resolutionFolder = ""
 let assemblyName = "FSharp.Data.Bson.dll"
 let runtimeAssembly = sourceDirectory ++ ".." ++ ".." ++ "bin" ++ assemblyName
-let portable47RuntimeAssembly = sourceDirectory ++ ".." ++ ".." ++ "bin" ++ "portable47" ++ assemblyName
 
 let generateAllExpected() =
     if not <| Directory.Exists expectedDirectory then
@@ -40,14 +39,9 @@ let normalize (str:string) =
 
 [<Test>]
 [<TestCaseSource "testCases">]
-let ``Validate signature didn't change `` (testCase:TypeProviderInstantiation) =
+let ``Validate signature didn't change`` (testCase:TypeProviderInstantiation) =
     let expected = testCase.ExpectedPath expectedDirectory |> File.ReadAllText |> normalize
     let output = testCase.Dump resolutionFolder "" runtimeAssembly (*signatureOnly*)false (*ignoreOutput*)false |> normalize
     if output <> expected then
         printfn "Obtained Signature:\n%s" output
     output |> should equal expected
-
-[<Test>]
-[<TestCaseSource "testCases">]
-let ``Generating expressions works in portable profile 47 `` (testCase:TypeProviderInstantiation) =
-    testCase.Dump resolutionFolder "" portable47RuntimeAssembly (*signatureOnly*)false (*ignoreOutput*)true |> ignore
