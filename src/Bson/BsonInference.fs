c0461e19077bb365ee3f0041c7800bf36cc2d028
Max Hirschhorn <max.hirschhorn@mongodb.com>
2014-09-01 20:12:03 -0400

    Clean up BsonInference.fs.

--- src/Bson/BsonInference.fs
+++ src/Bson/BsonInference.fs
@@ -6,12 +6,11 @@ module BsonProvider.ProviderImplementation.BsonInference
 
 open System
 open MongoDB.Bson
-open FSharp.Data
 open FSharp.Data.Runtime
 open FSharp.Data.Runtime.StructuralTypes
 
 /// Infer the type of a BSON value.
-let rec inferType parentName (bsonValue : BsonValue) =
+let rec inferType parentName (bsonValue:BsonValue) =
     match bsonValue.BsonType with
     | BsonType.Null -> InferedType.Null
     | BsonType.Boolean -> InferedType.Primitive (typeof<bool>, None, false)
@@ -20,28 +19,22 @@ let rec inferType parentName (bsonValue : BsonValue) =
     | BsonType.Double -> InferedType.Primitive (typeof<float>, None, false)
     | BsonType.String -> InferedType.Primitive (typeof<string>, None, false)
     | BsonType.DateTime -> InferedType.Primitive (typeof<DateTime>, None, false)
-
-    | BsonType.Binary -> InferedType.Primitive (typeof<BsonBinaryData>, None, false)
     | BsonType.ObjectId -> InferedType.Primitive (typeof<ObjectId>, None, false)
 
     | BsonType.Array elems ->
         let elemName = NameUtils.singularize parentName
-        let allowEmptyValues = false
-
         bsonValue.AsBsonArray
         |> Seq.map (inferType elemName)
-        |> StructuralInference.inferCollectionType allowEmptyValues
+        |> StructuralInference.inferCollectionType (*allowEmptyValues*)false
 
     | BsonType.Document elems ->
         let recordName =
-            if String.IsNullOrEmpty parentName
-            then None
+            if String.IsNullOrEmpty parentName then None
             else Some parentName
         let fields =
             [ for elem in bsonValue.AsBsonDocument ->
                 let typ = inferType elem.Name elem.Value
                 { InferedProperty.Name = elem.Name; Type = typ} ]
-
         InferedType.Record (recordName, fields, false)
 
     | _ -> InferedType.Primitive (typeof<BsonType>, None, false)
